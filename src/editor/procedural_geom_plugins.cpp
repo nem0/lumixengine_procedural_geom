7f6e10f17653f518d1f27c756499f8aa0f42acee
Mikulas Florek <mikulas.florek@gamedev.sk>
2022-11-07 00:25:58 +0100

    some basic functionality

--- src/editor/procedural_geom_plugins.cpp
+++ src/editor/procedural_geom_plugins.cpp
@@ -1,32 +1,647 @@
 #define LUMIX_NO_CUSTOM_CRT
+#include "editor/asset_browser.h"
+#include "editor/settings.h"
 #include "editor/studio_app.h"
+#include "editor/utils.h"
 #include "editor/world_editor.h"
 #include "engine/allocator.h"
+#include "engine/array.h"
+#include "engine/log.h"
+#include "engine/os.h"
+#include "engine/string.h"
+#include "engine/universe.h"
+#include "renderer/material.h"
+#include "renderer/render_scene.h"
+
 
 #include "imgui/imgui.h"
 
 
-using namespace Lumix;
+namespace Lumix {
+
+struct ProceduralGeomPlugin;
+
+namespace {
+
+enum { OUTPUT_FLAG = 1 << 31 };
+
+enum class NodeType : u32 { 
+	OUTPUT,
+	CUBE,
+	DISTRIBUTE_POINT_ON_FACES,
+	GRID,
+	TRANSFORM,
+	MERGE
+};
+
+struct Geometry {
+	struct Vertex {
+		Vec3 position;
+		Vec2 uv;
+		Vec3 normal;
+	};
+
+	Geometry(IAllocator& allocator)
+		: vertices(allocator)
+		, indices(allocator)
+	{}
+
+	Geometry&& move() { return static_cast<Geometry&&>(*this); }
+
+	Array<Vertex> vertices;
+	Array<u32> indices;
+	gpu::PrimitiveType type;
+};
+
+struct Node {
+	virtual void gui() = 0;
+
+	virtual NodeType getType() = 0;
+
+	virtual bool getGeometry(u16 output_idx, Geometry* result) = 0;
+	virtual void serialize(OutputMemoryStream& blob) {}
+	virtual void deserialize(InputMemoryStream& blob) {}
+
+	void nodeGUI() {
+		ImGuiEx::BeginNode(m_id, m_pos, &m_selected);
+		m_input_counter = 0;
+		m_output_counter = 0;
+		gui();
+		ImGuiEx::EndNode();
+	}
+
+	void inputSlot() {
+		ImGuiEx::Pin(m_id | ((u32)m_input_counter << 16), true);
+		++m_input_counter;
+	}
+
+	void outputSlot() {
+		ImGuiEx::Pin(m_id | ((u32)m_output_counter << 16) | OUTPUT_FLAG, false);
+		++m_output_counter;
+	}
+
+	ProceduralGeomPlugin* m_plugin;
+	IAllocator* m_allocator;
+	u16 m_id;
+	bool m_selected = false;
+	ImVec2 m_pos = ImVec2(0, 0);
+	u16 m_input_counter = 0;
+	u16 m_output_counter = 0;
+};
+
+struct Link {
+	u32 from;
+	u32 to;
+};
+
+struct Header {
+	static constexpr u32 MAGIC = '_PGM';
+	u32 magic = MAGIC;
+	u32 version = 0;
+};
+
+} // anonymous namespace
+
+static const ComponentType MODEL_INSTANCE_TYPE = reflection::getComponentType("model_instance");
 
 struct ProceduralGeomPlugin : StudioApp::GUIPlugin {
-	ProceduralGeomPlugin(StudioApp& app) : m_app(app) {}
+	ProceduralGeomPlugin(StudioApp& app)
+		: m_app(app)
+		, m_allocator(app.getAllocator())
+		, m_nodes(app.getAllocator())
+		, m_links(app.getAllocator())
+		, m_recent_paths(app.getAllocator())
+	{
+		Node* n = addNode(NodeType::OUTPUT);
+		n->m_pos = ImVec2(100, 100);
+
+		m_toggle_ui.init("Procedural editor", "Toggle procedural editor", "procedural_editor", "", true);
+		m_toggle_ui.func.bind<&ProceduralGeomPlugin::toggleOpen>(this);
+		m_toggle_ui.is_selected.bind<&ProceduralGeomPlugin::isOpen>(this);
+		app.addWindowAction(&m_toggle_ui);
+	}
+
+	~ProceduralGeomPlugin(){
+		m_app.removeAction(&m_toggle_ui);
+	}
+
+	bool isOpen() const { return m_is_open; }
+	void toggleOpen() { m_is_open = !m_is_open; }
+	bool hasFocus() override { return m_has_focus; }
+
+	void load(const char* path) {
+		os::InputFile file;
+		if (!file.open(path)) {
+			logError("Failed to load ", path);
+			return;
+		}
+
+		const u32 data_size = (u32)file.size();
+		OutputMemoryStream data(m_allocator);
+		data.resize(data_size);
+		if (!file.read(data.getMutableData(), data_size)) {
+			logError("Failed to load shader ", path);
+			file.close();
+			return;
+		}
+		file.close();
+
+		InputMemoryStream blob(data);
+		deserialize(blob, path);
+
+		pushRecent(path);
+	}
+
+	void pushRecent(const char* path) {
+		String p(path, m_app.getAllocator());
+		m_recent_paths.eraseItems([&](const String& s) { return s == path; });
+		m_recent_paths.push(static_cast<String&&>(p));
+	}
+
+	void load() {
+		ASSERT(false);
+	}
+
+	void clear() {
+		m_nodes.clear();
+		m_node_id_genereator = 1;
+		m_links.clear();
+	}
+	
+	void serialize(OutputMemoryStream& blob) {
+		Header header;
+		blob.write(header);
+		blob.writeString(m_material);
+		blob.write(m_nodes.size());
+		for (UniquePtr<Node>& node : m_nodes) {
+			blob.write(node->getType());
+			blob.write(node->m_id);
+			blob.write(node->m_pos);
+			node->serialize(blob);
+		}
+		blob.write(m_links.size());
+		blob.write(m_links.begin(), m_links.byte_size());
+	}
+
+	void deserialize(InputMemoryStream& blob, const char* path) {
+		clear();
+
+		Header header;
+		blob.read(header);
+		m_material = blob.readString();
+		if (header.magic != Header::MAGIC) {
+			logError("Corrupted file ", path);
+			return;
+		}
+		u32 count;
+		blob.read(count);
+		m_nodes.reserve(count);
+		for (u32 i = 0; i < count; ++i) {
+			NodeType type;
+			blob.read(type);
+			Node* node = addNode(type);
+			blob.read(node->m_id);
+			blob.read(node->m_pos);
+			node->deserialize(blob);
+		}
+		blob.read(count);
+		m_links.resize(count);
+		blob.read(m_links.begin(), m_links.byte_size());
+	}
+
+	void save() {
+		char path[LUMIX_MAX_PATH];
+		
+		if (!os::getSaveFilename(Span(path), "Procedural geometry\0*.pgm", "pgm")) return;
+		OutputMemoryStream blob(m_allocator);
+		serialize(blob);
+		
+		os::OutputFile file;
+		if(!file.open(path)) {
+			logError("Could not save ", path);
+			return;
+		}
+
+		bool success = file.write(blob.data(), blob.size());
+		file.close();
+		if (!success) {
+			logError("Could not save ", path);
+		}
+
+		pushRecent(path);
+	}
+
+	void onSettingsLoaded() override {
+		Settings& settings = m_app.getSettings();
+		m_is_open = settings.getValue(Settings::GLOBAL, "is_procedural_geom_editor_open", false);
+		char tmp[LUMIX_MAX_PATH];
+		m_recent_paths.clear();
+		for (u32 i = 0; ; ++i) {
+			const StaticString<32> key("procedural_geom_editor_recent_", i);
+			const u32 len = settings.getValue(Settings::LOCAL, key, Span(tmp));
+			if (len == 0) break;
+			m_recent_paths.emplace(tmp, m_app.getAllocator());
+		}
+	}
+
+	void onBeforeSettingsSaved() override {
+		Settings& settings = m_app.getSettings();
+		settings.setValue(Settings::GLOBAL, "is_procedural_geom_editor_open", m_is_open);
+		for (const String& p : m_recent_paths) {
+			const u32 i = u32(&p - m_recent_paths.begin());	
+			const StaticString<32> key("procedural_geom_editor_recent_", i);
+			settings.setValue(Settings::LOCAL, key, p.c_str());
+		}
+	}
 
 	void onWindowGUI() override {
+		m_has_focus = false;
+		if (!m_is_open) return;
+
 		ImGui::SetNextWindowSize(ImVec2(200, 200), ImGuiCond_FirstUseEver);
-		if (ImGui::Begin("Procedural geometry")) {
-			ImGui::TextUnformatted("Hello world");
+		if (ImGui::Begin("Procedural geometry", &m_is_open, ImGuiWindowFlags_MenuBar)) {
+			m_has_focus = ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows);		
+			if (ImGui::BeginMenuBar()) {
+				if (ImGui::BeginMenu("File")) {
+					if (ImGui::MenuItem("Load")) load();
+					if (ImGui::MenuItem("Save")) save();
+					if (ImGui::BeginMenu("Recet", !m_recent_paths.empty())) {
+						for (const String& path : m_recent_paths) {
+							if (ImGui::MenuItem(path.c_str())) load(path.c_str());
+						}
+						ImGui::EndMenu();
+					}
+					ImGui::EndMenu();
+				}
+				if (ImGui::MenuItem("Apply", nullptr, false, !m_material.empty())) apply();
+				ImGui::EndMenuBar();
+			}
+
+			m_app.getAssetBrowser().resourceInput("material", Span(m_material.data), Material::TYPE);
+
+			m_canvas.begin();
+			ImGuiEx::BeginNodeEditor("pg", &m_offset);
+			for (UniquePtr<Node>& node : m_nodes) {
+				node->nodeGUI();
+			}
+
+			i32 hovered_link = -1;
+			for (i32 i = 0, c = m_links.size(); i < c; ++i) {
+				const Link& link = m_links[i];
+				ImGuiEx::NodeLink(link.from | OUTPUT_FLAG, link.to);
+				if (ImGuiEx::IsLinkHovered()) {
+					hovered_link = i;
+				}
+			}
+
+			u32 newfrom, newto;
+			if (ImGuiEx::GetNewLink(&newfrom, &newto)) {
+				m_links.eraseItems([&](const Link& link){ return link.to == newto; });
+				m_links.emplace(Link{newfrom, newto});
+			}
+
+			ImGuiEx::EndNodeEditor();
+
+			if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(0)) {
+				if (ImGui::GetIO().KeyAlt && hovered_link != -1) {
+					m_links.erase(hovered_link);
+				}
+			}
+
+			if (ImGui::IsItemHovered() && ImGui::IsMouseClicked(1)) {
+				m_context_pos = ImGui::GetMousePos() - m_offset;
+				ImGui::OpenPopup("context_menu");
+			}
+
+			if(ImGui::BeginPopup("context_menu")) {
+				static char filter[64] = "";
+				
+				Node* new_node = nullptr;
+				if (ImGui::MenuItem("Transform")) new_node = addNode(NodeType::TRANSFORM);
+				if (ImGui::MenuItem("Cube")) new_node = addNode(NodeType::CUBE);
+				if (ImGui::MenuItem("Grid")) new_node = addNode(NodeType::GRID);
+				if (ImGui::MenuItem("Merge")) new_node = addNode(NodeType::MERGE);
+
+				if (new_node) new_node->m_pos = m_context_pos;
+
+				ImGui::EndPopup();
+			}		
+
+			m_canvas.end();
 		}
 		ImGui::End();
 	}
 	
+	void apply();
+	Node* addNode(NodeType type);
+
 	const char* getName() const override { return "procedural_geom"; }
 
+	u16 m_node_id_genereator = 1;
+	IAllocator& m_allocator;
+	Array<UniquePtr<Node>> m_nodes;
+	Array<Link> m_links;
 	StudioApp& m_app;
+	ImVec2 m_offset = ImVec2(0, 0);
+	ImGuiEx::Canvas m_canvas;
+	bool m_is_open = true;
+	StaticString<LUMIX_MAX_PATH> m_material;
+	Array<String> m_recent_paths;
+	ImVec2 m_context_pos;
+	bool m_has_focus = false;
+	Action m_toggle_ui;
 };
 
+static u16 toNodeId(int id) {
+	return u16(id);
+}
+
+static u16 toAttrIdx(int id) {
+	return u16(u32(id) >> 16);
+}
+
+template <typename F>
+static void	forEachInput(const ProceduralGeomPlugin& editor, int node_id, const F& f) {
+	for (const Link& link : editor.m_links) {
+		if (toNodeId(link.to) == node_id) {
+			const int iter = editor.m_nodes.find([&](const UniquePtr<Node>& node) { return node->m_id == toNodeId(link.from); }); 
+			Node* from = editor.m_nodes[iter].get();
+			const u16 from_attr = toAttrIdx(link.from);
+			const u16 to_attr = toAttrIdx(link.to);
+			f(from, from_attr, to_attr, u32(&link - editor.m_links.begin()));
+		}
+	}
+}
+
+struct Input {
+	Node* node = nullptr;
+	u16 output_idx;
+	bool getGeometry(Geometry* result) const { return node->getGeometry(output_idx, result); }
+	operator bool() const { return node != nullptr; }
+};
+
+static Input getInput(const ProceduralGeomPlugin& editor, u16 node_id, u16 input_idx) {
+	Input res;
+	forEachInput(editor, node_id, [&](Node* from, u16 from_attr, u16 to_attr, u32 link_idx){
+		if (to_attr == input_idx) {
+			res.output_idx = from_attr;
+			res.node = from;
+		}
+	});
+	return res;
+}
+
+struct CubeNode : Node {
+	NodeType getType() override { return NodeType::CUBE; }
+
+	bool getGeometry(u16 output_idx, Geometry* result) override {
+		const Geometry::Vertex cube_verts[] = {
+			{ {-1, -1, -1},		{0, 0}, {-1, -1, -1} },
+			{ { 1, -1, -1},		{1, 0}, {1, -1, -1}	},
+			{ { 1, -1, 1},		{1, 0}, { 1, -1, 1}	},
+			{ { -1, -1, 1},		{0, 0}, {-1, -1, 1}	},
+			{ { -1, 1, -1},		{0, 1}, { -1, 1, -1} },
+			{ { 1, 1, -1},		{1, 1}, { 1, 1, -1}	},
+			{ { 1, 1, 1},		{1, 1}, { 1, 1, 1} },
+			{ { -1, 1, 1},		{0, 1}, { -1, 1, 1}	},
+		};
+		result->vertices.resize(8);
+		memcpy(result->vertices.begin(), cube_verts, sizeof(cube_verts));
+
+		u16 cube_indices[] = {
+			0, 1, 2,
+			0, 2, 3,
+			4, 6, 5,
+			4, 7, 6,
+			0, 4, 5,
+			0, 5, 1,
+			2, 6, 7,
+			2, 7, 3,
+			0, 3, 7,
+			0, 7, 4,
+			1, 6, 2,
+			1, 5, 6
+		};		
+		result->indices.reserve(lengthOf(cube_indices));
+		for (u16 idx : cube_indices) result->indices.push(idx);
+		result->type = gpu::PrimitiveType::TRIANGLES;
+		return true;
+	}
+
+	void gui() override {
+		ImGuiEx::NodeTitle("Cube");
+		outputSlot();
+		ImGui::TextUnformatted("Geometry");
+	}
+};
+
+struct MergeNode : Node {
+	NodeType getType() override { return NodeType::MERGE; }
+
+	bool getGeometry(u16 output_idx, Geometry* result) override {
+		const Input input0 = getInput(*m_plugin, m_id, 0);
+		if (!input0) return false;
+		if (!input0.getGeometry(result)) return false;
+
+		const Input input1 = getInput(*m_plugin, m_id, 1);
+		if (input1) {
+			Geometry tmp(*m_allocator);
+			if (!input1.getGeometry(&tmp)) return false;
+			if (result->type != tmp.type) return false;
+			
+			const u32 old_size = result->vertices.size();
+
+			result->vertices.resize(result->vertices.size() + tmp.vertices.size());
+			memcpy(&result->vertices[old_size], tmp.vertices.begin(), tmp.vertices.byte_size());
+
+			result->indices.reserve(result->indices.size() + tmp.indices.size());
+			for (u32 idx : tmp.indices) {
+				result->indices.push(idx + old_size);
+			}
+		}
+
+		return true;
+	}
+
+	void gui() override {
+		ImGuiEx::NodeTitle("Merge");
+		ImGui::BeginGroup();
+		inputSlot(); ImGui::TextUnformatted("A");
+		inputSlot(); ImGui::TextUnformatted("B");
+		ImGui::EndGroup();
+		ImGui::SameLine();		
+		outputSlot();
+	}
+};
+
+struct TransformNode : Node {
+	NodeType getType() override { return NodeType::TRANSFORM; }
+
+	void serialize(OutputMemoryStream& blob) {
+		blob.write(translation);
+		blob.write(euler);
+		blob.write(scale);
+	}
+	
+	void deserialize(InputMemoryStream& blob) {
+		blob.read(translation);
+		blob.read(euler);
+		blob.read(scale);
+	}
+
+	bool getGeometry(u16 output_idx, Geometry* result) override {
+		const Input input = getInput(*m_plugin, m_id, 0);
+		if (!input) return false;
+		if (!input.getGeometry(result)) return false;
+
+		Quat rotation;
+		rotation.fromEuler(euler);
+
+		for (Geometry::Vertex& v : result->vertices) {
+			v.position = rotation.rotate(v.position) * scale + translation;
+		}
+
+		return true;
+	}
+
+	void gui() override {
+		ImGuiEx::NodeTitle("Transform");
+		inputSlot();
+		ImGui::BeginGroup();
+		ImGui::DragFloat3("Translation", &translation.x);
+		ImGuiEx::InputRotation("Rotation", &euler.x);
+		ImGui::DragFloat3("Scale", &scale.x);
+		ImGui::EndGroup();
+		ImGui::SameLine();
+		outputSlot();
+	}
+
+	Vec3 translation = Vec3::ZERO;
+	Vec3 euler = Vec3::ZERO;
+	Vec3 scale = Vec3(1);
+};
+
+struct GridNode : Node {
+	NodeType getType() override { return NodeType::GRID; }
+
+	bool getGeometry(u16 output_idx, Geometry* result) override {
+		result->type = gpu::PrimitiveType::TRIANGLES;
+		result->vertices.resize((m_rows + 1) * (m_cols + 1));
+		for (i32 j = 0; j < m_rows + 1; ++j) {
+			for (i32 i = 0; i < m_cols + 1; ++i) {
+				result->vertices[i + j * (m_cols + 1)] = {
+					{ i / float(m_cols), j / float(m_rows), 0 },
+					{ i / float(m_cols), j / float(m_rows) },
+					{0, 0, 1}
+				};
+			}
+		}
+
+		result->indices.reserve(m_rows * m_cols * 6);
+		for (i32 j = 0; j < m_rows; ++j) {
+			for (i32 i = 0; i < m_cols; ++i) {
+				result->indices.push(i + j * (m_cols + 1));
+				result->indices.push(i + 1 + j * (m_cols + 1));
+				result->indices.push(i + (j + 1) * (m_cols + 1));
+
+				result->indices.push(i + (j + 1) * (m_cols + 1));
+				result->indices.push(i + 1 + j * (m_cols + 1));
+				result->indices.push(i + 1 + (j + 1) * (m_cols + 1));
+			}
+		}
+
+		return true;
+	}
+
+	void gui() override {
+		ImGuiEx::NodeTitle("Grid");
+		outputSlot();
+		ImGui::DragInt("Columns", &m_cols);
+		ImGui::DragInt("Rows", &m_rows);
+	}
+
+	i32 m_cols = 10;
+	i32 m_rows = 10;
+};
+
+struct DistributePointsOnFacesNode : Node {
+	NodeType getType() override { return NodeType::DISTRIBUTE_POINT_ON_FACES; }
+
+	bool getGeometry(u16 output_idx, Geometry* result) override {
+		// TODO
+		return false;
+	}
+
+	void gui() override {
+		ImGuiEx::NodeTitle("Distribute points on faces");
+		inputSlot();
+		ImGui::TextUnformatted(" ");
+		ImGui::SameLine();
+		outputSlot();
+	}
+};
+
+struct OutputGeometryNode : Node {
+	NodeType getType() override { return NodeType::OUTPUT; }
+	
+	bool getGeometry(u16 output_idx, Geometry* result) override {
+		const Input input = getInput(*m_plugin, m_id, 0);
+		if (input) return input.getGeometry(result);
+		return false;
+	}
+
+	void gui() override {
+		ImGuiEx::NodeTitle("Output");
+		inputSlot();
+		ImGui::NewLine();
+	}
+};
+
+void ProceduralGeomPlugin::apply() {	
+	OutputGeometryNode* output = (OutputGeometryNode*)m_nodes[0].get();
+	Geometry geom(m_allocator);
+	if (!output->getGeometry(0, &geom)) return;
+
+	const Array<EntityRef>& selected = m_app.getWorldEditor().getSelectedEntities();
+	if (selected.size() != 1) return;
+
+	Universe* universe = m_app.getWorldEditor().getUniverse();
+	RenderScene* scene = (RenderScene*)universe->getScene("renderer");
+	if (!scene->hasProceduralGeometry(selected[0])) scene->createProceduralGeometry(selected[0]);
+
+	Span<const u8> vertices((const u8*)geom.vertices.begin(), (u32)geom.vertices.byte_size());
+	Span<const u8> indices((const u8*)geom.indices.begin(), (u32)geom.indices.byte_size());
+	gpu::VertexDecl decl;
+	decl.addAttribute(0, 0, 3, gpu::AttributeType::FLOAT, 0);
+	decl.addAttribute(1, 12, 2, gpu::AttributeType::FLOAT, 0);
+	decl.addAttribute(2, 20, 3, gpu::AttributeType::FLOAT, 0);
+	scene->setProceduralGeometry(selected[0], vertices, decl, geom.type, indices, gpu::DataType::U32);
+	scene->setProceduralGeometryMaterial(selected[0], Path(m_material));
+}
+
+Node* ProceduralGeomPlugin::addNode(NodeType type) {
+	UniquePtr<Node> node;
+	switch (type) {
+		case NodeType::CUBE: node = UniquePtr<CubeNode>::create(m_allocator); break;
+		case NodeType::OUTPUT: node = UniquePtr<OutputGeometryNode>::create(m_allocator); break;
+		case NodeType::DISTRIBUTE_POINT_ON_FACES: node = UniquePtr<DistributePointsOnFacesNode>::create(m_allocator); break;
+		case NodeType::GRID: node = UniquePtr<GridNode>::create(m_allocator); break;
+		case NodeType::TRANSFORM: node = UniquePtr<TransformNode>::create(m_allocator); break;
+		case NodeType::MERGE: node = UniquePtr<MergeNode>::create(m_allocator); break;
+		default: ASSERT(false); return nullptr;
+	}
+	Node* res = node.get();
+	res->m_plugin = this;
+	res->m_allocator = &m_allocator;
+	res->m_id = ++m_node_id_genereator;
+	m_nodes.push(node.move());
+	return res;
+}
 
 LUMIX_STUDIO_ENTRY(procedural_geom) {
 	auto* plugin = LUMIX_NEW(app.getAllocator(), ProceduralGeomPlugin)(app);
 	app.addPlugin(*plugin);
 	return nullptr;
 }
+
+} // namespace Lumix
